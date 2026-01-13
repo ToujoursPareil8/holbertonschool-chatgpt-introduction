f58fa8cc54f3331b86c6a68838d33cde457c58c1
ToujoursPareil8 <jasonaridy@gmail.com>
2026-01-13 18:31:03 +0100

    trying to fix unknown holberton issue

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-
 def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
+	result = 1
+	while n > 1:
+		result *= n
+		n -= 1  # Décrémente n pour éviter la boucle infinie
+	return result
 
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        f = factorial(int(sys.argv[1]))
-        print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
